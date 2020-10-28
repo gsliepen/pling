3b6153564e35c882669aa258d7da1515152dbc62
Guus Sliepen <guus@sliepen.org>
2020-10-28 20:47:41 +0100

    Fix missing #includes in header files.

--- src/envelopes/exponential-adsr.hpp
+++ src/envelopes/exponential-adsr.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <cmath>
+#include <string>
 
 #include "../pling.hpp"
 
--- src/filters/state-variable.hpp
+++ src/filters/state-variable.hpp
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <string>
 #include <glm/glm.hpp>
 #include "../pling.hpp"
 
